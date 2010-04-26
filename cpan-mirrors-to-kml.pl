600b86335e680dcde2e8ddef3b9e05c4f767fcfc
AHinMaine <aharrison@gmail.com>
2010-04-26 09:35:58 -0400

    Simplified and cleaned up code.  Added options to override the defaults.  Added a fetcher to grab a fresh copy of the mirrors file.

--- cpan-mirrors-to-kml.pl
+++ cpan-mirrors-to-kml.pl
@@ -1,42 +1,71 @@
-#!/usr/bin/perl 
+#!/opt/local/bin/perl
 
 use strict;
 use warnings;
 
-use Data::Dumper;
-use Parse::CPAN::MirroredBy;
-use XML::Writer;
+use URI;
 use IO::File;
+use XML::Writer;
+use Data::Dumper;
+use Getopt::Long;
+use HTTP::Request;
+use LWP::UserAgent;
+use Parse::CPAN::MirroredBy;
 
-my $mirror_file = '/tmp/MIRRORED.BY';
-my $kml_file    = '/tmp/MIRRORED.BY.kml';
+my $opts = {};
+$opts->{force} = 0;
 
-print "Output file = $kml_file\n";
+$opts->{mirrored_by_url} = 'ftp://ftp.funet.fi/pub/languages/perl/CPAN/MIRRORED.BY';
+$opts->{mirror_file}     = '/tmp/MIRRORED.BY';
+$opts->{kml_file}        = '/tmp/MIRRORED.BY.kml';
+
+GetOptions( $opts,
+                    'force!',
+                    'kml_file=s',
+                    'mirror_file=s',
+                    'mirrored_by_url=s',
+);
+
+fetch_mirrors_file({ mirrored_by_url => $opts->{mirrored_by_url},
+                     mirror_file     => $opts->{mirror_file} });
 
 my $parse  = [];
 my $parser = Parse::CPAN::MirroredBy->new();
 
-@$parse = $parser->parse_file( $mirror_file );
+@$parse = $parser->parse_file( $opts->{mirror_file} );
 
-# Sample
-#
+my $KMLOUT = IO::File->new( ">" . $opts->{kml_file} );
+my $schema = 'http://www.opengis.net/kml/2.2';
+my $writer = XML::Writer->new(
+                               OUTPUT      => $KMLOUT,
+                               DATA_INDENT => 4,
+                               DATA_MODE   => "true"
+                             );
 
-#  {
-#    'dst_ftp' => 'ftp://mirrors.localhost.net.ar/pub/mirrors/CPAN',
-#    'dst_contact' => 'localhost.net.ar;mirrors',
-#    'hostname' => 'localhost.net.ar',
-#    'frequency' => 'daily',
-#    'dst_timezone' => '-3',
-#    'dst_location' => 'Buenos Aires, Argentina, South America (-34.6 -58.45)',
-#    'dst_http' => 'http://cpan.localhost.net.ar',
-#    'dst_src' => 'ftp.funet.fi',
-#    'dst_organisation' => 'LocalHost',
-#    'dst_bandwidth' => '100Mbit'
-#  },
 
-my $mirrors = {};
+$writer->xmlDecl("UTF-8");
+$writer->startTag( 'kml', xmlns => $schema );
+$writer->startTag('Document');
 
 for (@$parse) {
+
+    # Sample
+    #
+    #  {
+    #    'dst_ftp' => 'ftp://mirrors.localhost.net.ar/pub/mirrors/CPAN',
+    #    'dst_contact' => 'localhost.net.ar;mirrors',
+    #    'hostname' => 'localhost.net.ar',
+    #    'frequency' => 'daily',
+    #    'dst_timezone' => '-3',
+    #    'dst_location' => 'Buenos Aires, Argentina, South America (-34.6 -58.45)',
+    #    'dst_http' => 'http://cpan.localhost.net.ar',
+    #    'dst_src' => 'ftp.funet.fi',
+    #    'dst_organisation' => 'LocalHost',
+    #    'dst_bandwidth' => '100Mbit'
+    #  },
+
+
+    $writer->startTag('Placemark');
 
     my $loc = $_->{dst_location};
 
@@ -55,40 +84,17 @@ for (@$parse) {
     my $long = $1;
     my $lat  = $2;
 
-    $mirrors->{ $_->{hostname} }->{hostname}    = $_->{hostname};
-    $mirrors->{ $_->{hostname} }->{description} = "$city, $state, $country";
-    $mirrors->{ $_->{hostname} }->{coordinates} = "$lat, $long";
-
-}
-
-my $OUT    = IO::File->new( ">" . $kml_file );
-my $schema = 'http://www.opengis.net/kml/2.2';
-my $writer = XML::Writer->new(
-                               OUTPUT      => $OUT,
-                               DATA_INDENT => 4,
-                               DATA_MODE   => "true"
-                             );
-
-
-$writer->xmlDecl("UTF-8");
-$writer->startTag( 'kml', xmlns => $schema );
-$writer->startTag('Document');
-
-for my $cur_mirror ( keys %$mirrors ) {
-
-    $writer->startTag('Placemark');
-
     $writer->startTag('name');
-    $writer->characters( $mirrors->{$cur_mirror}->{hostname} );
+    $writer->characters( $_->{hostname} );
     $writer->endTag('name');
 
     $writer->startTag('description');
-    $writer->characters( $mirrors->{$cur_mirror}->{description} );
+    $writer->characters("$city, $state, $country");
     $writer->endTag('description');
 
     $writer->startTag('Point');
     $writer->startTag('coordinates');
-    $writer->characters( $mirrors->{$cur_mirror}->{coordinates} );
+    $writer->characters("$lat, $long");
     $writer->endTag('coordinates');
     $writer->endTag('Point');
 
@@ -96,9 +102,51 @@ for my $cur_mirror ( keys %$mirrors ) {
 
 }
 
-
 $writer->endTag('Document');
 $writer->endTag('kml');
 $writer->end();
-$OUT->close();
+$KMLOUT->close();
+
+print 'Output file = ' . $opts->{kml_file} . "\n";
+
+# {{{ Fetch the file...
+#
+sub fetch_mirrors_file {
+
+    my $args = shift;
+
+    my $url = $args->{mirrored_by_url};
+    my $out = $args->{mirror_file};
+
+    if ( ! $opts->{force} && -f $out ) {
+
+        my $timestamp = ( stat($out) )[10];
+
+        my $age = time - $timestamp;
+
+        # If file is less than 3 days old, skip the fetch.
+        #
+        if ( $age <= 259200 ) {
+            print "Mirrors file is less than three days old, skipping fetch.\n";
+            return;
+        }
+
+    }
+
+    print "Fetching fresh MIRRORED.BY file...\n";
+
+    my $fetcher = LWP::UserAgent->new;
+    my $request = HTTP::Request->new( GET => $url );
+    my $fetched = $fetcher->request($request);
+
+    my $OUT = IO::File->new(">$out")
+        or die "Error writing to output file: $!\n";
+
+    print $OUT $fetched->content;
+
+    $OUT->close();
+
+} # }}}
+
+__END__
 
